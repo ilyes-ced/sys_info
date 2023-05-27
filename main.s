3130ba534ca3b0e6b9e3a62a14d109ffd159f887
i-cant-find-a-good-username <random_dude_233@proton.me>
2023-05-27 21:35:02 +0200

    c tsets

---
+++ main.s
@@ -0,0 +1,32 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello World!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$5, -4(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.1.1 20230429"
+	.section	.note.GNU-stack,"",@progbits
---
+++ src/start.asm
@@ -0,0 +1,34 @@
+# comment
+
+.global _start
+.section .text
+
+_start:
+    mov r15, #8
+
+loop:
+    cmp r15, #0
+    beq exit
+
+    bl hello_world
+
+    sub r15, r15, #1
+    b loop
+
+
+hello_world:
+    mov r7, #0x4
+    mov r0, #1
+    ldr r1, =message
+    mov r2, #13
+    swi 0
+
+
+exit:
+    mov r7, #0x1
+    mov r0, #65
+    swi 0
+
+.section .data
+    message:
+    .ascii "hello world\n"
