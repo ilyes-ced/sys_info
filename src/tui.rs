ee0c321f04337fd3864bed50667555447f9a2035
noot-noot-pengu <random_dude_233@proton.me>
2023-07-24 20:14:29 +0100

    memory

--- src/tui.rs
+++ src/tui.rs
@@ -1,15 +1,45 @@
-pub mod tui_logic;
+mod tui_logic;
+mod utils;
 
-fn main () {
-	//let output = Command::new("neofetch").output();
-	//match output {
-	//    Ok(result) => {
-	//        println!("{}", String::from_utf8_lossy(&result.stdout));
-	//    }
-	//    Err(_) => {
-	//        println!("neofetch is required for this");
-	//    }
-	//}
 
-	let gg = tui_logic::main::main().unwrap();
+#[cfg(windows)]
+mod windows {
+    // include Windows-specific file here
+}
+#[cfg(unix)]
+mod system::linux;
+
+
+fn main() {
+    //let output = Command::new("neofetch").output();
+    //match output {
+    //    Ok(result) => {
+    //        println!("{}", String::from_utf8_lossy(&result.stdout));
+    //    }
+    //    Err(_) => {
+    //        println!("neofetch is required for this");
+    //    }
+    //}
+
+    //let gg = tui_logic::main::main().unwrap();
+
+
+
+
+
+        	
+    if cfg!(target_os = "linux") {
+        mod system;
+	} else if cfg!(target_os = "windows") {
+		mod system::linux;
+	} else if cfg!(target_os = "macos") {
+		mod system::linux;
+	} else {
+		mod system::linux;
+	}
+
+
+
+
+
 }
---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use std::{path::Path, fs::File, io::Read};
+
+
+pub fn read_file(path: &Path) -> Result<String, ()> {
+    let mut string = String::new();
+    let mut file = match File::open(path){
+        Ok(the_file) => the_file,
+        Err(_) => return Err(())
+    };
+
+    match file.read_to_string(&mut string){
+        Ok(_) => return Ok(string),
+        Err(_) =>return Err(())
+    }
+}
